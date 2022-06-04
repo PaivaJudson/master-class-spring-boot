2ad5211af4b0a0612fe89a782c5ea4b4ed8ac7df
judsonpaiva <judson.paiva@hotmail.com>
2022-06-04 12:14:49 +0100

    first commit spring boot

---
+++ src/main/java/com/judsonpaiva/workshop/MasterClassApplication.java
@@ -0,0 +1,13 @@
+package com.judsonpaiva.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterClassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/judsonpaiva/workshop/MasterClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.judsonpaiva.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
