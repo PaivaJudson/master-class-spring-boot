8ba445f4fda8cd0f9f782eb73e6b53448e738644
judsonpaiva <judson.paiva@hotmail.com>
2022-06-04 15:33:35 +0100

    Service layer, component registration

--- src/main/java/com/judsonpaiva/workshop/resources/UserResource.java
+++ src/main/java/com/judsonpaiva/workshop/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.judsonpaiva.workshop.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.judsonpaiva.workshop.entities.User;
+import com.judsonpaiva.workshop.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Leonor Paiva", "leonor@gmail.com", "+244 999", "12345");
-		return ResponseEntity.ok().body(u);
-	}	
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user);
+	}
 }
---
+++ src/main/java/com/judsonpaiva/workshop/services/UserService.java
@@ -0,0 +1,27 @@
+package com.judsonpaiva.workshop.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.judsonpaiva.workshop.entities.User;
+import com.judsonpaiva.workshop.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> user = repository.findById(id);
+		return user.get();
+	}
+	
+}
