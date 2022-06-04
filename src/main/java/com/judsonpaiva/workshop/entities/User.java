9fcd9687c36ce2b5cb9e6ef4eec3aee821d6c6a8
judsonpaiva <judson.paiva@hotmail.com>
2022-06-04 16:07:04 +0100

    Order, Instant, ISO 860

--- src/main/java/com/judsonpaiva/workshop/entities/User.java
+++ src/main/java/com/judsonpaiva/workshop/entities/User.java
@@ -1,11 +1,14 @@
 package com.judsonpaiva.workshop.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -22,6 +25,8 @@ public class User implements Serializable{
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {}
 
@@ -74,6 +79,10 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -98,11 +107,6 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
-	
-	
-	
-	
+
 	
 }
