55e5a138734a7ebbd6a8d9773eebf077a3360cf6
judsonpaiva <judson.paiva@hotmail.com>
2022-06-08 21:58:50 +0100

    Many-to-many association with JoinTable

--- src/main/java/com/judsonpaiva/workshop/entities/Product.java
+++ src/main/java/com/judsonpaiva/workshop/entities/Product.java
@@ -8,15 +8,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name = "products")
-public class Product implements Serializable{
+public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -24,12 +26,14 @@ public class Product implements Serializable{
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	@Transient
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
-	public Product() {}
-	
+
+	public Product() {
+	}
+
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		this.id = id;
 		this.name = name;
@@ -53,7 +57,7 @@ public class Product implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String getDescription() {
 		return description;
 	}
@@ -82,8 +86,6 @@ public class Product implements Serializable{
 		return categories;
 	}
 
-
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -91,8 +93,6 @@ public class Product implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -110,7 +110,5 @@ public class Product implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
 
 }
