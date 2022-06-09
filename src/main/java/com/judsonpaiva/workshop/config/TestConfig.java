36b0bb9aea2a589b67c9391cbe5fdb6f6aa15162
judsonpaiva <judson.paiva@hotmail.com>
2022-06-10 00:41:47 +0100

    OrderItem, many-to-many association with extra attributes parte - 1 continuacao

--- src/main/java/com/judsonpaiva/workshop/config/TestConfig.java
+++ src/main/java/com/judsonpaiva/workshop/config/TestConfig.java
@@ -67,5 +67,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 	}	
 }
